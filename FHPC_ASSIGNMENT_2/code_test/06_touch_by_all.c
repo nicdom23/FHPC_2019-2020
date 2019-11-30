2aa5839470a17e06d4c6b821424800f393738257
nicdom23 <nicoladomenis23@hotmail.it>
2019-11-30 14:12:56 +0100

    ordered folders

--- FHPC_ASSIGNMENT_2/code_test/06_touch_by_all.c
+++ FHPC_ASSIGNMENT_2/code_test/06_touch_by_all.c
@@ -30,29 +30,43 @@
 #     define _XOPEN_SOURCE 700
 #  endif
 #endif
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sched.h>
 #include <omp.h>
 
 
-#define N_default 100
+#define N_default 1000
 
 #if defined(_OPENMP)
 #define CPU_TIME (clock_gettime( CLOCK_REALTIME, &ts ), (double)ts.tv_sec + \
 		  (double)ts.tv_nsec * 1e-9)
 
-#define CPU_TIME_th (clock_gettime( CLOCK_THREAD_CPUTIME_ID, &myts ), (double)myts.tv_sec + \
+#define CPU_TIME_th (clock_gettime( CLOCK_THREAD_CPUTIME_ID, &myts ), (double)myts.tv_sec +	\
 		     (double)myts.tv_nsec * 1e-9)
+
 #else
 
 #define CPU_TIME (clock_gettime( CLOCK_PROCESS_CPUTIME_ID, &ts ), (double)ts.tv_sec + \
-		   (double)ts.tv_nsec * 1e-9)
-
+		  (double)ts.tv_nsec * 1e-9)
 #endif
 
+#ifdef OUTPUT
+#define PRINTF(...) printf(__VA_ARGS__)
+#else
+#define PRINTF(...)
+#endif
 
+#define CPU_ID_ENTRY_IN_PROCSTAT 39
+#define HOSTNAME_MAX_LENGTH      200
+
+int read_proc__self_stat ( int, int * );
+int get_cpu_id           ( void       );
 
 
 
@@ -65,8 +79,6 @@ int main( int argc, char **argv )
   struct  timespec ts;
   double *array;
 
-
-
   /*  -----------------------------------------------------------------------------
    *   initialize 
    *  -----------------------------------------------------------------------------
@@ -76,34 +88,36 @@ int main( int argc, char **argv )
   if ( argc > 1 )
     N = atoi( *(argv+1) );
 
-
-  // allocate memory
-  if ( (array = (double*)calloc( N, sizeof(double) )) == NULL )
-    {
-      printf("I'm sorry, there is not enough memory to host %lu bytes\n", N * sizeof(double) );
-      return 1;
-    }
-
+  if ( (array = (double*)malloc( N * sizeof(double) )) == NULL ) {
+    printf("I'm sorry, on some thread there is not"
+	   "enough memory to host %lu bytes\n",
+	   N * sizeof(double) ); return 1; }
+  
   // just give notice of what will happen and get the number of threads used
-#ifndef _OPENMP
-  printf("serial summation\n");
-#else
+#if defined(_OPENMP)  
 #pragma omp parallel
   {
 #pragma omp master
     {
       nthreads = omp_get_num_threads();
-      printf("omp summation with %d threads\n", nthreads );
+      PRINTF("omp summation with %d threads\n", nthreads );
     }
+    int me = omp_get_thread_num();
+#pragma omp critical
+    PRINTF("thread %2d is running on core %2d\n", me, get_cpu_id() );    
   }
 #endif
 
-  // initialize the array
-  srand48( time(NULL) ); //setting a non default seed value
+  // initialize the array;
+  // each thread is "touching"
+  // its own memory as long as
+  // the parallel for has the
+  // scheduling as the final one
+
+#pragma omp parallel for
   for ( int ii = 0; ii < N; ii++ )
-    array[ii] = (double)ii+1;                                 // choose the initialization you prefer;
-    //array[ii] = drand48();                                // the first one (with integers) makes it
-                                                            // to check the result
+    array[ii] = (double)ii;
+
 
 
   /*  -----------------------------------------------------------------------------
@@ -112,16 +126,15 @@ int main( int argc, char **argv )
    */
 
 
-  double S           = 0;                                   // this will store the summation
+  double S       = 0;                                       // this will store the summation
   double th_avg_time = 0;                                   // this will be the average thread runtime
-  double th_min_time = 0;                                   // this will be the min thread runtime.
+  double th_min_time = 1e11;                                // this will be the min thread runtime.
 							    // contrasting the average and the min
 							    // time taken by the threads, you may
 							    // have an idea of the unbalance.
-double th_max_time=0;
-	
-  double tstart  = CPU_TIME;
   
+  double tstart  = CPU_TIME;
+
 #if !defined(_OPENMP)
   
   for ( int ii = 0; ii < N; ii++ )                          // well, you may notice this implementation
@@ -129,8 +142,7 @@ double th_max_time=0;
 
 #else
 
-  
-#pragma omp parallel reduction(+:th_avg_time) \
+#pragma omp parallel reduction(+:th_avg_time)				\
   reduction(min:th_min_time)                                // in this region there are 2 different
   {                                                         // reductions: the one of runtime, which
     struct  timespec myts;                                  // happens in the whole parallel region;
@@ -142,28 +154,107 @@ double th_max_time=0;
     double mytime = CPU_TIME_th - mystart; 
     th_avg_time += mytime;
     th_min_time  = (mytime < th_min_time)? mytime : th_min_time;
-    th_max_time = (mytime>th_max_time)? mytime: th_max_time;
   }
 
 #endif
+  
+  double tend = CPU_TIME;
 
-  double tend = CPU_TIME;                                   // this timer is CLOCK_REALTIME if OpenMP
-							    // is active; CLOCK_PROCESS_CPU_TIME_ID
-							    // otherwise. That is because the latter
-							    // would accounts for the whole cpu time
-							    // used by the threads under OpenMP.
 
   /*  -----------------------------------------------------------------------------
    *   finalize
    *  -----------------------------------------------------------------------------
    */
 
-printf("Sum is %g, process took %g of wall-clock time\n\n"
-       "<%g> sec of avg thread-time\n"
-       "<%g> sec of min thread-time\n"
-	"<%g> sec of max thread-time\n",
-       S, tend - tstart, th_avg_time/nthreads, th_min_time ,th_max_time);
+  printf("Sum is %g, process took %g of wall-clock time\n\n"
+	 "<%g> sec of avg thread-time\n"
+	 "<%g> sec of min thread-time\n",
+	 S, tend - tstart, th_avg_time/nthreads, th_min_time );
+
+
+free( array );
+
+return 0;
+}
+
+
+
+
+
+
+int get_cpu_id( void )
+{
+#if defined(_GNU_SOURCE)                              // GNU SOURCE ------------
   
-  free( array );
+  return  sched_getcpu( );
+
+#else
+
+#ifdef SYS_getcpu                                     //     direct sys call ---
+  
+  int cpuid;
+  if ( syscall( SYS_getcpu, &cpuid, NULL, NULL ) == -1 )
+    return -1;
+  else
+    return cpuid;
+  
+#else      
+
+  unsigned val;
+  if ( read_proc__self_stat( CPU_ID_ENTRY_IN_PROCSTAT, &val ) == -1 )
+    return -1;
+
+  return (int)val;
+
+#endif                                                // -----------------------
+#endif
+
+}
+
+
+
+int read_proc__self_stat( int field, int *ret_val )
+/*
+  Other interesting fields:
+
+  pid      : 0
+  father   : 1
+  utime    : 13
+  cutime   : 14
+  nthreads : 18
+  rss      : 22
+  cpuid    : 39
+
+  read man /proc page for fully detailed infos
+ */
+{
+  // not used, just mnemonic
+  // char *table[ 52 ] = { [0]="pid", [1]="father", [13]="utime", [14]="cutime", [18]="nthreads", [22]="rss", [38]="cpuid"};
+
+  *ret_val = 0;
+
+  FILE *file = fopen( "/proc/self/stat", "r" );
+  if (file == NULL )
+    return -1;
+
+  char   *line = NULL;
+  int     ret;
+  size_t  len;
+  ret = getline( &line, &len, file );
+  fclose(file);
+
+  if( ret == -1 )
+    return -1;
+
+  char *savetoken = line;
+  char *token = strtok_r( line, " ", &savetoken);
+  --field;
+  do { token = strtok_r( NULL, " ", &savetoken); field--; } while( field );
+
+  *ret_val = atoi(token);
+
+  free(line);
+
   return 0;
 }
+
