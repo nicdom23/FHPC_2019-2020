e44f4da80b46af30dd1849ff08b4eeb73bde7d32
Nicola Domenis <nicoladomenis23@hotmail.it>
2019-12-04 22:53:39 +0100

    end of session

--- Workspace_2/execute2.sh
+++ Workspace_2/execute2.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#PBS -l nodes=2:ppn=10
-#PBS -N touch_by_ALL_TEST_w_2cores_20_nodes
+#PBS -l nodes=3:ppn=17
+#PBS -N touch_by_ALL_TEST_w_3cores_51_nodes
 #PBS -l walltime=00:01:00
 cd $HOME/FHPC_2019-2020/Workspace_2
 
-gcc -fopenmp -std=gnu99 -o touch_by_all 04_touch_by_all.x
+
+gcc -fopenmp -std=gnu99 -o touch_by_all 04_touch_by_all.c
+
 unset OMP_NUM_THREADS
-/usr/bin/time ./touch_by_all 1000000000 -> result/ex2on2nodesand20cores.txt
+/usr/bin/time ./touch_by_all 1000000000 -> results/ex2on3nodesand51cores.txt
  
--- Workspace_2/execute3.sh
+++ Workspace_2/execute3.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=10
-#PBS -N touch_by_ALL_TEST_w_one_node
+#PBS -l nodes=3:ppn=20
+#PBS -N touch_by_ALL_TEST_w_3nodes_60proceses
 #PBS -l walltime=00:01:00
 cd $HOME/FHPC_2019-2020/Workspace_2
 
 gcc -fopenmp -std=gnu99 -o touch_by_all 04_touch_by_all.c
 
-unset OMP_NUM_THREADS
-/usr/bin/time ./touch_by_all 1000000000 -> result/ex1on1nodes10cores.txt
+export OMP_NUM_THREADS=40
+/usr/bin/time ./touch_by_all 1000000000 -> results/ex3on3nodes60cores.txt
  
