62d80ba50bb7cbaf288b1f56e288f5f21518b89e
Nicola Domenis <nicoladomenis23@hotmail.it>
2019-12-07 22:49:05 +0100

    made comparison measurements

--- Workspace_2/pi_code/omp_comparison_test.sh
+++ Workspace_2/pi_code/omp_comparison_test.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=20
-#PBS -N weak_scalability_TEST_w_20_nodes_openmp_pi
+#PBS -N comparison_TEST_w_20_nodes_openmp_pi
 #PBS -l walltime=00:05:00
 cd $HOME/FHPC_2019-2020/Workspace_2/pi_code
-Var=time_execution_for_comparison.csv
+Var=compare/time_execution_for_comparison.csv
 
-rm Var
- touch Var
- echo "N;p;time" > Var
+rm $Var
+ touch $Var
+ echo "N;time" > $Var
 
  N=1000000
  NSHOTS=10
-
- for procs in 1 2 4 6 8 10 12 14 16 18 20 
+ export OMP_NUM_THREADS=20
+ for procs in 1 10 20 30 70 100 150 500 1000 1500 3000 
  do
         avg=0;
  	for (( i=0; i<=$NSHOTS; i=i+1 )) 
         do
- 		export OMP_NUM_THREADS=$procs
-		echo "Execution $i for p=$procs"
+ 	
+		echo "Execution $i for size=$(($procs*$N))"
 		#/usr/bin/time -f "buuuh %E" ./array_sum $N
 		realtime=$( /usr/bin/time -f "buuuh %E" ./openmp_pi $(($N*$procs)) 2>&1 | grep buuuh | cut -d' ' -f2) 
-		echo $realtime
+		echo -n "realtime :" $realtime
 		realminutes=$(( $(echo $realtime | cut -d':' -f1)*60 ))
 		realseconds=$(echo $realtime | cut -d':' -f2)
  		avg=$( echo "scale=4; $avg+$realminutes+$realseconds" | bc -l ) 
-		echo "     procs " $procs " avg = " $avg 
+		echo "     size " $(($N*$procs)) " avg = " $avg 
  	done
-        echo -n "avg time for procs " $procs " : "
+        echo -n "avg time for size " $(($N*$procs)) " : "
         echo "scale=4; $avg/($NSHOTS+1)" | bc
         
-        echo -n $N";"$procs";" >> time_executions_weak.csv
-        echo "scale=4; $avg/($NSHOTS+1)" | bc >> time_executions_weak.csv
+        echo -n $(($N*$procs))";" >> $Var
+        echo "scale=4; $avg/($NSHOTS+1)" | bc >> $Var
  done
 
