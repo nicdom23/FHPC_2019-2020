561f2e327cbb007303a76b0ce570e9c5c4e88091
Nicola Domenis <nicoladomenis23@hotmail.it>
2019-12-09 21:01:50 +0100

    added new measurations

--- Workspace_2/pi_code/mpi_pi_code/execution_of_code_mpi.sh
+++ Workspace_2/pi_code/mpi_pi_code/execution_of_code_mpi.sh
@@ -4,13 +4,12 @@
 #PBS -N weak_scalability_TEST_w_1_nodes_openmp_pi
 #PBS -l walltime=00:05:00
 cd $HOME/FHPC_2019-2020/Workspace_2/pi_code/mpi_pi_code
-VAR=results/time_executions.csv
- 
-rm $VAR
- touch $VAR
- echo "N;p;time" > $VAR
 
- N=500000000
+Var=parts_results/execution_code_mpi.csv
+ touch $Var
+ echo "N;time" > $Var
+
+ N=50000000
  NSHOTS=10
 
  for procs in 1 2 4 6 8 10 12 14 16 18 20 
@@ -19,7 +18,7 @@ rm $VAR
  	for (( i=0; i<=$NSHOTS; i=i+1 )) 
         do
  		echo "Execution $i for p=$procs"
-		realtime=$( /usr/bin/time -f "buuuh %E" mpirun -np ${procs} mpi_pi $N 2>&1 | grep buuuh | cut -d' ' -f2) 
+		realtime=$( /usr/bin/time -f "buuuh %E" mpirun -np 20 ./mpi_pi $(($N*$procs)) 2>&1 | grep buuuh | cut -d' ' -f2) 
 		realminutes=$(( $(echo $realtime | cut -d':' -f1)*60 ))
 		realseconds=$(echo $realtime | cut -d':' -f2)
  		avg=$( echo "scale=4; $avg+$realminutes+$realseconds" | bc -l ) 
@@ -28,6 +27,6 @@ rm $VAR
         echo -n "avg time for procs " $procs " : "
         echo "scale=4; $avg/($NSHOTS+1)" | bc
         
-        echo -n $N";"$procs";" >> $VAR
+        echo -n $(($N*$procs))"; >> $VAR
         echo "scale=4; $avg/($NSHOTS+1)" | bc >> $VAR
  done
