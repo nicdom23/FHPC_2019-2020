caba6df1fa87f6e4dca94ecfadf406b5e30d35da
Nicola Domenis <nicoladomenis23@hotmail.it>
2019-12-10 11:04:58 +0100

    recalculated once again elapsedand walltime in couple_of_times.csv

--- Workspace_2/pi_code/execution_of_all_times_code.sh
+++ Workspace_2/pi_code/execution_of_all_times_code.sh
@@ -11,29 +11,28 @@ touch $Var
 touch $Var2
  echo "N;time" > $Var
  echo "N;time" > $Var2
- M=50000000
+ N=50000000
  NSHOTS=10
 
- for size in 1 2 4 6 8 10 12 14 16 18 20
+ for procs in 1 2 4 6 8 10 12 14 16 18 20
  do
-	N=$(($M * $size))  
-      avg=0;
+        avg=0;
  	for (( i=0; i<=$NSHOTS; i=i+1 )) 
         do
  		export OMP_NUM_THREADS=20
 		echo "Execution $i for p=$procs"
 		#/usr/bin/time -f "buuuh %E" ./array_sum $N
-		realtime=$( /usr/bin/time -f "buuuh  %E" ./openmp_pi $N 2>&1 | grep buuuh | cut -d' ' -f2) 
+		realtime=$( /usr/bin/time -f "buuuh$procs  %E" ./openmp_pi $(($N*$procs)) 2>&1 | grep buuuh$procs | cut -d' ' -f2) 
 		echo $realtime
-		#realminutes=$(( $(echo $realtime | cut -d':' -f1)*60 ))
+		realminutes=$(( $(echo $realtime | cut -d':' -f1)*60 ))
 		realseconds=$(echo $realtime | cut -d':' -f2)
  		avg=$( echo "scale=4; $avg+$realminutes+$realseconds" | bc -l ) 
-		echo "     procs " $size " avg = " $avg 
+		echo "     procs " $procs " avg = " $avg 
  	done
-        echo -n "avg time for procs " $size " : "
+        echo -n "avg time for procs " $procs " : "
         echo "scale=4; $avg/($NSHOTS+1)" | bc
         
-        echo -n $N "," >> $Var
+        echo -n $(($N*$procs))"," >> $Var
         echo "scale=4; $avg/($NSHOTS+1)" | bc >> $Var
  done
 
