52be6796f12932ace012f3b46e900b521fcf0adc
Nicola Domenis <nicoladomenis23@hotmail.it>
2019-12-11 12:42:10 +0100

    premerge

--- Workspace_2/pi_code/openmp_pi.c
+++ Workspace_2/pi_code/openmp_pi.c
@@ -36,25 +36,22 @@ double x, y ;
 	
 	
 	myid = omp_get_thread_num();
+	printf("hello from thread %d from %d \n",myid,numprocs);
 	S =atoll(argv[1]);
 	N = S /numprocs;
-       //PRINTF("thread %d of %d \n",myid,numprocs);
- 
-  
-
-  
-
-  
+      
   unsigned int myseed=(SEED*(myid+1)) ; 
   local_M=0;
   long long int i;  
+
+/*
+//initialization and estmation of the parallel points
 #pragma omp parallel for reduction(+:local_M) 
 for (i=0; i<N ; i++) {  
     x = rand_r(&myseed)/(double)RAND_MAX; 
     y = rand_r(&myseed)/(double)RAND_MAX;    
 	if ((x*x + y*y)<1)
       local_M++;
- // PRINTF("local threads %lld from thread %d   \n",local_M,myid);
 	}
 
 end_time_1=omp_get_wtime();
@@ -63,20 +60,18 @@ start_time = omp_get_wtime();
 M += local_M ;
 
 end_time=omp_get_wtime();
-
-
-//PRINTF ( "\n # walltime on thread %i : %10.8f \n",myid, end_time - start_time ) ; 
+ 
 }
 
 //print for the comparison with mpi_pi.c
-//PRINTF ( "\n%d , %10.8f , %10.8f \n",S,end_time_1 - start_time_1, end_time - start_time ) ;
+PRINTF ("problem size, points estimation time, total sum time\n");
+PRINTF ( "%d , %10.8f , %10.8f \n",S,end_time_1 - start_time_1, end_time - start_time ) ;
 //print for walltime estimation
-PRINTF ( "\n%d , %10.8f \n",S,end_time - start_time_1 ) ;
-
-//PRINTF("N*numprocs= %lld,%d",N,numprocs);
+PRINTF("Walltime");
+PRINTF ( " %10.8f \n",end_time - start_time_1 ) ;
 pi = 4.0*M/(N*numprocs) ;
-  //PRINTF ( "\n # of trials = %llu , estimate of pi is %1.9f \n", N*numprocs, pi ) ;
-
- 	
+  PRINTF ( "\n # of trials = %llu , estimate of pi is %1.9f \n", N*numprocs, pi ) ;
+*/
+} 	
 return 0;
 }
