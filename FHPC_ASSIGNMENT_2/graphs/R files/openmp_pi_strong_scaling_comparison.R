b85f9da18a0c647d4db4472ea6945da48a4c7f40
nicdom23 <nicoladomenis23@hotmail.it>
2019-12-13 11:56:05 +0100

    finished assignment

--- FHPC_ASSIGNMENT_2/graphs/R files/openmp_pi_strong_scaling_comparison.R
+++ FHPC_ASSIGNMENT_2/graphs/R files/openmp_pi_strong_scaling_comparison.R
@@ -21,14 +21,14 @@ legend(13, 20, legend = c("walltime","elapsed time"), fill = c("red","blue"), co
 
 #4 plots on the same box
 
-plot(strong_scalability_elapsed_time_openmp$p,strong_scalability_elapsed_time_openmp$time, col="blue",type="l",main="strong scalability execution times openmp_pi")
+plot(strong_scalability_elapsed_time_openmp$p,strong_scalability_elapsed_time_openmp$time,ylab="time", xlab="p", col="blue",type="l",main="strong scalability execution times")
 
 
 lines(strong_scalability_walltime_openmp$p,strong_scalability_walltime_openmp$time,col="red",type="l",main="strong scalability walltime openmp")
-lines(p_mpi_strongscaling,times_walltimes_mpi,type="l",col="red",main="strong scaling execution times mpi_pi")
-lines(strong_scaling_elapsed_time_mpi$p,strong_scaling_elapsed_time_mpi$time,col="blue",type="l",main="strong scaling elapsed times mpi")
+lines(p_mpi_strongscaling,times_walltimes_mpi,type="l",col="green",main="strong scaling execution times mpi_pi")
+lines(strong_scaling_elapsed_time_mpi$p,strong_scaling_elapsed_time_mpi$time,col="violet",type="l",main="strong scaling elapsed times mpi")
 
-legend(13, 20, legend = c("walltime","elapsed time"), fill = c("red","blue"), col="red", bg ="white")
+legend(14, 20, legend = c("openmp walltime","openmp elapsed time","mpi_pi walltime","mpi_pi elapsed_time"), fill = c("red","blue","green","violet"), col="red", bg ="white")
 
 
 legend(13, 20, legend = c("walltime","elapsed time"), fill = c("red","blue"), col="red", bg ="white")
